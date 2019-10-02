2b929c177eaff52c59573683f3de685972bdcfb2
Mike Thiem <mikethiem@MikeThiem-MacBook.local>
2019-10-02 15:05:17 +0200

    testy

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400)
+  background(150,200,50
+  )
+}
+
+function draw() {
+  circle(100,200,200)
+  circle(200,200,200)
+  // put drawing code here
+}
